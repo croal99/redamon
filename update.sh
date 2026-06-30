5e73ad2feae9cda0cd143e5319cbdcc767c06a18
xp <xp@mail.com>
2026-07-01 02:15:15 +0800

    anythingllm

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 git submodule update --init --remote anythingllm
+cp anythingllm/.env.example anythingllm/.env
+# 设置权限（与你的 UID:GID 匹配，通常是 1000:1000）
+sudo chown -R 1000:1000 anythingllm/storage/
+sudo chown -R 1000:1000 anythingllm/.env
